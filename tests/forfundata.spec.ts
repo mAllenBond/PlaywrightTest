81c66131a7c5ef888615d96bf9fc4296a21a55a5
Michael Allen-Bond <mbond@securecodewarrior.com>
2023-01-20 14:20:07 -0800

    Create base spec file
    Co-authored-by: Gregory Whorley <gwhorley@securecodewarrior.com>
    Co-authored-by: Jack Truong <jtruong@securecodewarrior.com>

--- tests/forfundata.spec.ts
+++ tests/forfundata.spec.ts
@@ -1,18 +1,48 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('Has expected title', async ({ page }) => {
   await page.goto('https://forfundataproject.web.app/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle("ForFun");
 });
 
-test('Has Table', async ({ page }) => {
+test("Can click into a user's posts", async ({ page }) => {
+  await page.goto('https://forfundataproject.web.app/');
+  await page.content();
+
+  // Pick a user and click on the posts
+  const post = await page.locator('mat-expansion-panel').first();
+  const postButton = await post.getByRole("button");
+
+  // Expand the first post row
+  await postButton.click();
+
+  // Get the columns locator for the first post
+  const postTableColumns = post.getByRole('columnheader');
+
+  // NOTE:  MUST WAIT FOR AT LEAST ONE OF THE COLUMNS TO BE VISIBLE BEFORE YOU CAN USE THE LOCATOR
+  await postTableColumns.first().waitFor({state:"visible"})
+
+  // Expect table to render with Title, Content, and Comments columns
+  expect(await postTableColumns.count()).toEqual(3);
+
+  expect(await postTableColumns.nth(0).innerText()).toEqual('Title');
+  expect(await postTableColumns.nth(1).innerText()).toEqual('Content');
+  expect(await postTableColumns.nth(2).innerText()).toEqual('Comments');
+});
+
+test("Can click into a post's comments", async ({ page }) => {
   await page.goto('https://forfundataproject.web.app/');
 
-  // Click the get started link.
-  await page.getByRole('button', { name: 'Get started' }).click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+  // TODO:  Fill in test here
+  // Can we leverage any ids on the elements?
 });
+
+test('Has expected number of distinct users', async ({ page }) => {
+  await page.goto('https://forfundataproject.web.app/');
+
+  // TODO:  Fill in test here
+});
+
+// TODO:  What other tests do you think would be worthwhile to add here
